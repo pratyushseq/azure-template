6d2a062657a4d6a79817da033c07f0a9f8c46e15
Pratyush Poddar <pratyush.poddar@sequoia.com>
2024-02-08 10:29:43 +0530

    change image link to sequoia cdn

--- script.mjs
+++ script.mjs
@@ -4,12 +4,9 @@ const main = (await fs.readFile("./src/index.html")).toString();
 const testing = (await fs.readFile("./src/testing.html")).toString();
 const styles = (await fs.readFile("./src/styles.css")).toString();
 const script = (await fs.readFile("./src/script.js")).toString();
-const image = await fs.readFile("./assets/home-bg.png");
-const imageBase64 = Buffer.from(image).toString("base64");
 
 const formatHtml = (html) =>
   html
-    .replace("{{BG_IMAGE}}", `data:image/png;base64,${imageBase64}`)
     .replace("{{STYLES}}", `<style>${styles}</style>`)
     .replace("{{SCRIPTS}}", `<script>${script}</script>`);
 
